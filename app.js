2bc6adb8b4aabd655db3bc8d2eed6f27231bb490
Adam Romines <adam.romines@gmail.com>
2015-10-22 14:21:41 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var color = "white";
+var allColors = 'red blue green yellow white';
+var mouseDown = false;
+$('.box').on('click', function() {
+  $(this).removeClass(allColors).addClass(color);
+}).on('mousedown',function(){
+  mouseDown = true;
+  // console.log('mouseDown: ' + mouseDown);
+}).on('mouseup', function(){
+  mouseDown = false;
+  // console.log('mouseDown: ' + mouseDown);
+}).on('mouseenter', function(){
+  if (mouseDown) {
+    $(this).removeClass(allColors).addClass(color);
+  }
+});
+
+$('.box').on('dblclick', function() {
+  $(this).removeClass('red blue green yellow white');
+});
+$('#reset').on('click', function() {
+  $('.box').removeClass('red blue green yellow white');
+});
+$('#red').on('click', function() {
+  color = 'red';
+});
+
+$('#blue').on('click', function() {
+  color = 'blue';
+});
+
+$('#green').on('click', function() {
+  color = 'green';
+});
+
+$('#yellow').on('click', function() {
+  color = 'yellow';
+});
+
+$('#white').on('click', function() {
+  color = 'white';
+});
